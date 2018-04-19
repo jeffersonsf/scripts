580ae8902c0141fdca1daf6b34cb2684c547b2c6
jeffersonsf <jeffersonferreira167@gmail.com>
2018-04-19 11:59:28 -0300

    Update preventiva.sh

--- preventiva.sh
+++ preventiva.sh
@@ -2,24 +2,51 @@
 
 echo "PREVENTIVA SUMMIT..";
 sleep 2;
-
-
-
 echo "################";
 echo "Coletando informações";
+sleep 2;
 
+dt=`date "+%d%m%Y"`;
+
+###################Verifica espaço em disco######################################
 disco=$(df -h | awk '{print $5}' | sed '1,3d' | sed 's/%//g');
-up=$(uptime);
-memoria=$(free -m);
-data=$(date);
-nome=$(hostname);
+
+#Cria Array com os valores coletados de todas as partições 
+a=0;
+for i in ${disco[@]}
+do
+	array[$a]=$i;
+	a=$((a+1));
+done
+
+##Verifica se o disco está acima ou não
+
+for t in ${array[*]}
+do
+	if [ $t -ge 90 ]; #Se entrar nessa condição o disco irá estourar
+	then
+		echo "######VERIFICAR ESPAÇO DISCO#######" >> /root/report_$dt
+		df -h | tee -a  >> /root/report_$dt
+		break;	
+	fi
+done
+
+################ Verifica tempo maquina ligada ############################
+
+time=`uptime | awk '{print $3}'`;
+if [ $time -le 7 ];
+then 
+	echo  "Maquina reiniciada a menos de 7 dias" >> /root/report_$dt;
+	uptime | tee -a  >> /root/report_$dt;
+fi
+
+############## Verifica Uso de memoria ################
+
+##Verificar através do SAR 
+
+################ Verificar tamanho do banco de dados ##################
 
 
-for i in $(seq 10); do echo $disco | cut -d " " -f$i; done
 
-##Faz Verificações
 
-if [ $disco -ge 90 ]
-then
-	echo "Disco vai estourar"; 
-fi  
+
