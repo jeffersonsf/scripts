ecf031e689ac6b11eab03d1e1f7724fad847fdc6
jeffersonsf <jeffersonferreira167@gmail.com>
2018-06-21 10:43:08 -0300

    Listando versões dos serviços corretamente

--- check_version_services.sh
+++ check_version_services.sh
@@ -1,45 +1,43 @@
 #!/bin/bash
 Menu(){
-	echo "";
-	echo "[ 0 ] Para verificar todos os serviços instalados em todas as máquinas: ";
-	echo "[ 1 ] Para verificar um serviço especifico em todas as máquinas: ";
-	echo "[ 3 ] Sair";
-	echo "";
-	echo " Qual a opçpção desejada: ";
-	read opcao;
-	case $opcao in
-		0)TodosSer ;;
-		1)UmSerTodas ;;
-		3)Sair ;;
-		*) echo "Opcao desconhecida." ; echo ; Menu ;
+        echo "";
+        echo "[ 0 ] Para verificar todos os serviços instalados em todas as máquinas: ";
+        echo "[ 1 ] Para verificar um serviço especifico em todas as máquinas: ";
+        echo "[ 3 ] Sair";
+        echo "";
+        read -p "Qual a opção desejada: " opcao;
+        case $opcao in
+                0)TodosSer ;;
+                1)UmSerTodas ;;
+                3)Sair ;;
+                *) echo "Opcao desconhecida." ; echo ; Menu ;
 
-	esac
+        esac
 }
 TodosSer(){
 echo -e "";
 ##Utilizando o hosts cadastrados dentro do arquivo /etc/hosts, realiza a verificação dos serviços summit
-for i in $(cat /etc/hosts | awk '{print $2}' | egrep -v '^$'| grep -v 'localhost'); 
+for i in $(cat /etc/hosts | awk '{print $2}' | egrep -v '^$'| egrep -v 'localhost|summit|');
 do
-	echo "";
-	echo "Servidor: $i"; 
-	ssh $i "ls -l /opt/sn |awk '{print \$9}'|egrep [1-9]";
+        echo "";
+        echo "Servidor: $i";
+        ssh $i "ls -l /opt/sn | grep '>'| awk '{print \$11}'|sed 's#/opt/sn/##g'";
 done
 Menu;
 }
 UmSerTodas(){
-	echo "Qual serviço você quer verificar?";
-	read service;
-	echo -e "";
+        read -p "Qual serviço você quer verificar? " service;
+        echo -e "";
 ##Utilizando o hosts cadastrados dentro do arquivo /etc/hosts, realiza a verificação dos serviços summit
-for i in $(cat /etc/hosts | awk '{print $2}' | egrep -v '^$'| grep -v 'localhost'); 
+for i in $(cat /etc/hosts | awk '{print $2}' | egrep -v '^$'| egrep -v 'localhost|summit');
 do
-	echo "";
-	echo "Servidor: $i"; 
-	ssh $i "ls -l /opt/sn |awk '{print \$9}'|egrep "[1-9]"| grep -i '$service'";
+        echo "";
+        echo "Servidor: $i";
+        ssh $i "ls -l /opt/sn | grep '>' | awk '{print \$11}'| sed 's#/opt/sn/##g'|grep -i $service";
 done
 Menu;
 }
 Sair(){
-	exit;
+        exit;
 }
 Menu;
