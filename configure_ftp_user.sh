97c61c27e0ecf210f6d2e10e3afe7de0cd5fe568
jeffersonsf <jeffersonferreira167@gmail.com>
2018-07-30 15:28:22 -0300

    Alteracao de owner e set directory
    Alteracao de owner e set directory

--- configure_ftp_user.sh
+++ configure_ftp_user.sh
@@ -1,27 +1,25 @@
 #!/bin/bash 
-
 ##Verificação da existência do Usuário ou se o mesmo está em branco
 op0=true;
 while [[ $op0 == true ]]; do
-	echo -e "\nDigite o usuário que deverá ser criado a conta FTP";
-	read newUser;
-	existUser=`getent passwd $newUser | wc -l`;
-	if [[ -z $newUser  ]]; then
-		echo "Usuario em branco, digite um usuario valido!";	
-	elif [[ $existUser -ge 1 ]]; then
-		echo "Usuario já existe, digite outro usuario";				
-	else
-		op0=false;
-	fi
+        echo -e "\nDigite o usuário que deverá ser criado a conta FTP";
+        read newUser;
+        existUser=`getent passwd $newUser | wc -l`;
+        if [[ -z $newUser  ]]; then
+                echo "Usuario em branco, digite um usuario valido!";
+        elif [[ $existUser -ge 1 ]]; then
+                echo "Usuario já existe, digite outro usuario";
+        else
+                op0=false;
+        fi
 done
 ##Criação de usuários e configuração dos diretórios
-cd /rislab;
-mkdir $newUser;
-useradd -d $directory/$newUser -s /sbin/nologin $newUser;
+directory=/rislab/$newUser;
+useradd -d $directory -s /sbin/nologin $newUser;
 pass=`date | md5sum | head -c8`;
 usermod -p $(openssl passwd $pass) $newUser;
+chown $newUser. /rislab/$newUser;
 echo "";
 echo "Usuario inserido com sucesso, abaixo os dados para acesso via FTP:";
 echo "Usuario: $newUser";
 echo "Senha: $pass";
-
